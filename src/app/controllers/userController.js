d77a611e4a61e002af87f12e911b73c2a6eceedc
seonuk <sunwook317@gmail.com>
2020-11-01 16:29:49 +0900

    일반 회원가입 및 로그인 구현

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,182 +1,291 @@
-const {pool} = require('../../../config/database');
-const {logger} = require('../../../config/winston');
+const { pool } = require("../../../config/database");
+const { logger } = require("../../../config/winston");
 
-const jwt = require('jsonwebtoken');
-const regexEmail = require('regex-email');
-const crypto = require('crypto');
-const secret_config = require('../../../config/secret');
+const jwt = require("jsonwebtoken");
+const regexEmail = require("regex-email");
+const regexBirth = /\d{4}\.\d{2}\.\d{2}/;
+const crypto = require("crypto");
+const secret_config = require("../../../config/secret");
 
-const userDao = require('../dao/userDao');
-const { constants } = require('buffer');
+const userDao = require("../dao/userDao");
+const { constants } = require("buffer");
 
 /**
- update : 2020.10.4
- 01.signUp API = 회원가입
+ update : 2020.11.1
+ 02.signUp API = 회원가입
  */
-exports.signUp = async function (req, res) {
-    const {
-        email, password, nickname
-    } = req.body;
+exports.signUp = async function(req, res) {
+    const { email, pwd, nickname, birth } = req.body;
 
-    if (!email) return res.json({isSuccess: false, code: 301, message: "이메일을 입력해주세요."});
-    if (email.length > 30) return res.json({
-        isSuccess: false,
-        code: 302,
-        message: "이메일은 30자리 미만으로 입력해주세요."
-    });
+    if (!email)
+        return res.status(400).json({
+            isSuccess: false,
+            code: 3,
+            message:
+                "Body Parameter Error : 파라미터 'email'이 존재하지 않습니다."
+        });
 
-    if (!regexEmail.test(email)) return res.json({isSuccess: false, code: 303, message: "이메일을 형식을 정확하게 입력해주세요."});
+    if (!pwd)
+        return res.status(400).json({
+            isSuccess: false,
+            code: 4,
+            message:
+                "Body Parameter Error : 파라미터 'pwd'가 존재하지 않습니다."
+        });
 
-    if (!password) return res.json({isSuccess: false, code: 304, message: "비밀번호를 입력 해주세요."});
-    if (password.length < 6 || password.length > 20) return res.json({
-        isSuccess: false,
-        code: 305,
-        message: "비밀번호는 6~20자리를 입력해주세요."
-    });
-
-    if (!nickname) return res.json({isSuccess: false, code: 306, message: "닉네임을 입력 해주세요."});
-    if (nickname.length > 20) return res.json({
-        isSuccess: false,
-        code: 307,
-        message: "닉네임은 최대 20자리를 입력해주세요."
-    });
-
-    try {
-        try {
-            // 이메일 중복 확인
-            const emailRows = await userDao.userEmailCheck(email);
-            if (emailRows.length > 0) {
-
-                return res.json({
-                    isSuccess: false,
-                    code: 308,
-                    message: "중복된 이메일입니다."
-                });
-            }
-
-            // 닉네임 중복 확인
-            const nicknameRows = await userDao.userNicknameCheck(nickname);
-            if (nicknameRows.length > 0) {
-                return res.json({
-                    isSuccess: false,
-                    code: 309,
-                    message: "중복된 닉네임입니다."
-                });
-            }
-
-            // TRANSACTION : advanced
-           // await connection.beginTransaction(); // START TRANSACTION
-            const hashedPassword = await crypto.createHash('sha512').update(password).digest('hex');
-            const insertUserInfoParams = [email, hashedPassword, nickname];
-            
-            const insertUserRows = await userDao.insertUserInfo(insertUserInfoParams);
-
-          //  await connection.commit(); // COMMIT
-           // connection.release();
-            return res.json({
-                isSuccess: true,
-                code: 200,
-                message: "회원가입 성공"
-            });
-        } catch (err) {
-           // await connection.rollback(); // ROLLBACK
-           // connection.release();
-            logger.error(`App - SignUp Query error\n: ${err.message}`);
-            return res.status(500).send(`Error: ${err.message}`);
-        }
-    } catch (err) {
-        logger.error(`App - SignUp DB Connection error\n: ${err.message}`);
-        return res.status(500).send(`Error: ${err.message}`);
+    if (!nickname) {
+        return res.status(400).json({
+            isSuccess: false,
+            code: 5,
+            message:
+                "Body Parameter Error : 파라미터 'nickname'이 존재하지 않습니다."
+        });
     }
-};
 
-/**
- update : 2020.10.4
- 02.signIn API = 로그인
- **/
-exports.signIn = async function (req, res) {
-    const {
-        email, password
-    } = req.body;
+    if (!birth) {
+        return res.status(400).json({
+            isSuccess: false,
+            code: 6,
+            message:
+                "Body Parameter Error : 파라미터 'birth'가 존재하지 않습니다."
+        });
+    }
 
-    if (!email) return res.json({isSuccess: false, code: 301, message: "이메일을 입력해주세요."});
-    if (email.length > 30) return res.json({
-        isSuccess: false,
-        code: 302,
-        message: "이메일은 30자리 미만으로 입력해주세요."
-    });
+    if (email.length >= 30 || !regexEmail.test(email))
+        return res.status(400).json({
+            isSuccess: false,
+            code: 7,
+            message:
+                "Body Parameter Error : 'email' 형식이 잘못되었습니다. (30자 미만, 이메일 정규표현 지키기)"
+        });
 
-    if (!regexEmail.test(email)) return res.json({isSuccess: false, code: 303, message: "이메일을 형식을 정확하게 입력해주세요."});
+    if (pwd.length < 10 || pwd.length >= 20)
+        return res.status(400).json({
+            isSuccess: false,
+            code: 8,
+            message:
+                "비밀번호는 6~20자리를 Body Parameter Error : 'pwd' 형식이 잘못되었습니다. (10자 이상 20자 미만)."
+        });
 
-    if (!password) return res.json({isSuccess: false, code: 304, message: "비밀번호를 입력 해주세요."});
+    if (nickname.length < 2 || nickname.length >= 10) {
+        return res.status(400).json({
+            isSuccess: false,
+            code: 9,
+            message:
+                "Body Parameter Error : 'nickname' 형식이 잘못되었습니다. (2자 이상 10자 미만)"
+        });
+    }
+
+    if (!regexBirth.test(birth)) {
+        return res.status(400).json({
+            isSuccess: false,
+            code: 10,
+            message:
+                "Body Parameter Error : 'birth' 형식이 잘못되었습니다. (yyyy.mm.dd 형식)"
+        });
+    }
 
     try {
         const connection = await pool.getConnection(async conn => conn);
 
         try {
-            const [userInfoRows] = await userDao.selectUserInfo(email)
+            // 이메일 중복 확인
+            const existObj = await userDao.userEmailCheck(email, connection);
 
-            if (userInfoRows.length < 1) {
-                connection.release();
-                return res.json({
+            if (existObj.exist) {
+                return res.status(400).json({
                     isSuccess: false,
-                    code: 310,
-                    message: "아이디를 확인해주세요."
+                    code: 2,
+                    message: "이미 존재하는 회원입니다."
                 });
             }
 
-            const hashedPassword = await crypto.createHash('sha512').update(password).digest('hex');
-            if (userInfoRows[0].pswd !== hashedPassword) {
-                connection.release();
+            await connection.beginTransaction(); // START TRANSACTION
+
+            const hashedPassword = await crypto
+                .createHash("sha512")
+                .update(pwd)
+                .digest("hex");
+
+            const insertUserInfoParams = [
+                email,
+                hashedPassword,
+                nickname,
+                birth
+            ];
+
+            const insertUserRows = await userDao.insertUserInfo(
+                insertUserInfoParams,
+                connection
+            );
+
+            const idx = insertUserRows.insertId;
+            const user = await userDao.selectUserInfoByIdx(idx, connection);
+
+            if (user.email === email) {
+                await connection.commit(); // COMMIT
+
+                const token = await jwt.sign(
+                    {
+                        idx: idx
+                    }, // 토큰의 내용(payload)
+                    secret_config.jwtsecret, // 비밀 키
+                    {
+                        expiresIn: "365d",
+                        subject: "userInfo"
+                    } // 유효 시간은 365일
+                );
+
                 return res.json({
+                    result: {
+                        email: user.email,
+                        jwt: token
+                    },
+                    isSuccess: true,
+                    code: 1,
+                    message: "회원가입 성공"
+                });
+            } else {
+                return res.status(500).json({
                     isSuccess: false,
-                    code: 311,
-                    message: "비밀번호를 확인해주세요."
+                    code: 500,
+                    message: "서버 에러 : 문의 요망"
                 });
             }
-            if (userInfoRows[0].status === "INACTIVE") {
-                connection.release();
+        } catch (err) {
+            await connection.rollback(); // ROLLBACK
+            logger.error(`App - SignUp DB Connection error\n: ${err.message}`);
+            return res.status(500).json({
+                isSuccess: false,
+                code: 500,
+                message: "서버 에러 : 문의 요망"
+            });
+        } finally {
+            connection.release();
+        }
+    } catch (err) {
+        logger.error(`App - SignUp DB Connection error\n: ${err.message}`);
+        return res.status(500).json({
+            isSuccess: false,
+            code: 500,
+            message: "서버 에러 : 문의 요망"
+        });
+    }
+};
+
+/**
+ update : 2020.11.1
+ 01.login API = 로그인
+ **/
+exports.login = async function(req, res) {
+    const { email, pwd } = req.body;
+
+    if (!email)
+        return res.json({
+            isSuccess: false,
+            code: 4,
+            message:
+                "Body Parameter Error : 파라미터 'email'이 존재하지 않습니다."
+        });
+
+    if (email.length >= 30 || !regexEmail.test(email))
+        return res.json({
+            isSuccess: false,
+            code: 6,
+            message:
+                "Body Parameter Error : 'email' 형식이 잘못되었습니다. (30자 미만, 이메일 정규표현식)"
+        });
+
+    if (!pwd)
+        return res.json({
+            isSuccess: false,
+            code: 5,
+            message:
+                "Body Parameter Error : 파라미터 'pwd'가 존재하지 않습니다."
+        });
+
+    if (pwd.length < 10 || pwd.length >= 20)
+        return res.json({
+            isSuccess: false,
+            code: 7,
+            message:
+                "Body Parameter Error : 'pwd' 형식이 잘못되었습니다. (10자 이상 20자 미만)"
+        });
+
+    try {
+        const connection = await pool.getConnection(async conn => conn);
+
+        try {
+            const userInfoRow = await userDao.selectUserInfo(email, connection);
+
+            if (userInfoRow.length < 1) {
                 return res.json({
                     isSuccess: false,
-                    code: 312,
-                    message: "비활성화 된 계정입니다. 고객센터에 문의해주세요."
+                    code: 2,
+                    message: "회원 정보를 찾을 수 없습니다."
                 });
-            } else if (userInfoRows[0].status === "DELETED") {
-                connection.release();
+            }
+
+            const hashedPassword = await crypto
+                .createHash("sha512")
+                .update(pwd)
+                .digest("hex");
+
+            if (userInfoRow[0].pwd !== hashedPassword) {
                 return res.json({
                     isSuccess: false,
-                    code: 313,
-                    message: "탈퇴 된 계정입니다. 고객센터에 문의해주세요."
+                    code: 3,
+                    message: "email과 password가 부합하지 않습니다."
+                });
+            }
+
+            if (userInfoRow[0].isDeleted) {
+                return res.json({
+                    isSuccess: false,
+                    code: 8,
+                    message: "탈퇴된 계정입니다."
                 });
             }
             //토큰 생성
-            let token = await jwt.sign({
-                    id: userInfoRows[0].id,
+            const token = await jwt.sign(
+                {
+                    idx: userInfoRow[0].idx
                 }, // 토큰의 내용(payload)
                 secret_config.jwtsecret, // 비밀 키
                 {
-                    expiresIn: '365d',
-                    subject: 'userInfo',
+                    expiresIn: "365d",
+                    subject: "userInfo"
                 } // 유효 시간은 365일
             );
 
-            res.json({
-                userInfo: userInfoRows[0],
-                jwt: token,
+            res.status(200).json({
+                result: {
+                    email: userInfoRow[0].email,
+                    jwt: token
+                },
                 isSuccess: true,
-                code: 200,
+                code: 1,
                 message: "로그인 성공"
             });
-
-            connection.release();
         } catch (err) {
             logger.error(`App - SignIn Query error\n: ${JSON.stringify(err)}`);
+            return res.status(500).json({
+                isSuccess: false,
+                code: 500,
+                message: "서버 에러 : 문의 요망"
+            });
+        } finally {
             connection.release();
-            return false;
         }
     } catch (err) {
-        logger.error(`App - SignIn DB Connection error\n: ${JSON.stringify(err)}`);
-        return false;
+        logger.error(
+            `App - SignIn DB Connection error\n: ${JSON.stringify(err)}`
+        );
+        return res.status(500).json({
+            isSuccess: false,
+            code: 500,
+            message: "서버 에러 : 문의 요망"
+        });
     }
 };
 
@@ -184,11 +293,11 @@ exports.signIn = async function (req, res) {
  update : 2019.09.23
  03.check API = token 검증
  **/
-exports.check = async function (req, res) {
+exports.check = async function(req, res) {
     res.json({
         isSuccess: true,
         code: 200,
         message: "검증 성공",
         info: req.verifiedToken
-    })
+    });
 };
--- src/app/dao/userDao.js
+++ src/app/dao/userDao.js
@@ -1,73 +1,74 @@
 const { pool } = require("../../../config/database");
 
 // Signup
-async function userEmailCheck(email) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const selectEmailQuery = `
-                SELECT email, nickname 
-                FROM UserInfo 
-                WHERE email = ?;
+async function userEmailCheck(email, connection) {
+    const selectEmailQuery = `
+    SELECT EXISTS(SELECT email FROM user WHERE email = ?) as exist;
                 `;
-  const selectEmailParams = [email];
-  const [emailRows] = await connection.query(
-    selectEmailQuery,
-    selectEmailParams
-  );
-  connection.release();
+    const selectEmailParams = [email];
+    const [existRow] = await connection.query(
+        selectEmailQuery,
+        selectEmailParams
+    );
 
-  return emailRows;
+    return existRow[0];
 }
 
 async function userNicknameCheck(nickname) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const selectNicknameQuery = `
+    const connection = await pool.getConnection(async conn => conn);
+    const selectNicknameQuery = `
                 SELECT email, nickname 
                 FROM UserInfo 
                 WHERE nickname = ?;
                 `;
-  const selectNicknameParams = [nickname];
-  const [nicknameRows] = await connection.query(
-    selectNicknameQuery,
-    selectNicknameParams
-  );
-  connection.release();
-  return nicknameRows;
+    const selectNicknameParams = [nickname];
+    const [nicknameRows] = await connection.query(
+        selectNicknameQuery,
+        selectNicknameParams
+    );
+    connection.release();
+    return nicknameRows;
 }
 
-async function insertUserInfo(insertUserInfoParams) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const insertUserInfoQuery = `
-        INSERT INTO UserInfo(email, pswd, nickname)
-        VALUES (?, ?, ?);
-    `;
-  const insertUserInfoRow = await connection.query(
-    insertUserInfoQuery,
-    insertUserInfoParams
-  );
-  connection.release();
-  return insertUserInfoRow;
+async function insertUserInfo(insertUserInfoParams, connection) {
+    const insertUserInfoQuery = `INSERT INTO user(email, pwd, nickname, birth) VALUES (?, ?, ?, ?);`;
+    const [insertUserInfoRow] = await connection.query(
+        insertUserInfoQuery,
+        insertUserInfoParams
+    );
+    console.log(insertUserInfoRow);
+
+    return insertUserInfoRow;
 }
 
 //SignIn
-async function selectUserInfo(email) {
-  const connection = await pool.getConnection(async (conn) => conn);
-  const selectUserInfoQuery = `
-                SELECT id, email , pswd, nickname, status 
-                FROM UserInfo 
-                WHERE email = ?;
-                `;
+async function selectUserInfo(email, connection) {
+    const selectUserInfoQuery = `SELECT idx, email, isDeleted, pwd FROM user WHERE email = ?;`;
 
-  let selectUserInfoParams = [email];
-  const [userInfoRows] = await connection.query(
-    selectUserInfoQuery,
-    selectUserInfoParams
-  );
-  return [userInfoRows];
+    const selectUserInfoParams = [email];
+    const [userInfoRows] = await connection.query(
+        selectUserInfoQuery,
+        selectUserInfoParams
+    );
+    return userInfoRows;
+}
+
+async function selectUserInfoByIdx(idx, connection) {
+    const selectUserInfoQuery = `SELECT email FROM user WHERE idx = ?;`;
+    const selectUserInfoParams = [idx];
+
+    const [userInfoRows] = await connection.query(
+        selectUserInfoQuery,
+        selectUserInfoParams
+    );
+
+    return userInfoRows[0];
 }
 
 module.exports = {
-  userEmailCheck,
-  userNicknameCheck,
-  insertUserInfo,
-  selectUserInfo,
+    userEmailCheck,
+    userNicknameCheck,
+    insertUserInfo,
+    selectUserInfo,
+    selectUserInfoByIdx
 };
