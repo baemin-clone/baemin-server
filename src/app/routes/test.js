7d52d89009102314984694163b1badc9db8e251e
seonuk <sunwook317@gmail.com>
2020-11-09 01:30:43 +0900

    AWS S3 파일 업로드 모듈 구현

--- src/app/routes/test.js
+++ src/app/routes/test.js
@@ -1,5 +1,6 @@
 const { pool } = require("config/database");
 
+const { upload } = require("modules/fileUpload");
 module.exports = function(app) {
     app.get("/users", async (req, res) => {
         const connection = await pool.getConnection(async conn => conn);
@@ -14,6 +15,19 @@ module.exports = function(app) {
             res.send(err);
         } finally {
             connection.release();
+        }
+    });
+
+    app.post("/upload", upload.single("img"), (req, res) => {
+        try {
+            console.log("req.file: ", req.file);
+
+            let payload = { uri: req.file.location };
+            res.json({
+                payload
+            });
+        } catch (err) {
+            console.log(err);
         }
     });
 };
